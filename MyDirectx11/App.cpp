879e355b4c02ad44050031e5cc0be32cbe833962
suha1515 <44773554+suha1515@users.noreply.github.com>
2019-11-08 19:39:29 +0900

    텍스쳐 매핑

--- MyDirectx11/App.cpp
+++ MyDirectx11/App.cpp
@@ -41,8 +41,8 @@ App::App()
 					gfx, rng, adist, ddist,
 					odist, rdist, longdist, latdist
 					);
-			//case 3:
-				//return std::make_unique<Sheet>(gfx, rng, adist, ddist, odist, rdist);
+			case 3:
+				return std::make_unique<Sheet>(gfx, rng, adist, ddist, odist, rdist);
 			default:
 				assert(false && "bad drawable type in factory");
 				return {};
@@ -58,7 +58,7 @@ App::App()
 		std::uniform_real_distribution<float> bdist{ 0.4f,3.0f };
 		std::uniform_int_distribution<int> latdist{ 5,20 };
 		std::uniform_int_distribution<int> longdist{ 10,40 };
-		std::uniform_int_distribution<int> typedist{ 0,2 };
+		std::uniform_int_distribution<int> typedist{ 0,3 };
 	};
 	
 	Factory f(wnd.Gfx());
