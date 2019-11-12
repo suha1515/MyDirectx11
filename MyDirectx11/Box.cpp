58d7ef67e6de388239dc53f7fdb2e4d6d836585c
suha1515 <44773554+suha1515@users.noreply.github.com>
2019-11-12 11:03:45 +0900

    정반사 추가작업

--- MyDirectx11/Box.cpp
+++ MyDirectx11/Box.cpp
@@ -64,8 +64,10 @@ Box::Box(Graphics& gfx,
 
 	struct PSMaterialConstant
 	{
-		dx::XMFLOAT3 color;
-		float padding;
+		alignas(16)dx::XMFLOAT3 color;
+		float specularIntensiry = 0.6f;
+		float specularPower = 30.f;
+		float padding[2];
 	} colorConst;
 	colorConst.color = material;
 	AddBind(std::make_unique<PixelConstantBuffer<PSMaterialConstant>>(gfx, colorConst, 1u));
--- MyDirectx11/PhongPS.hlsl
+++ MyDirectx11/PhongPS.hlsl
@@ -1,17 +1,20 @@
  cbuffer LightBuf
 {
-	float3 lightPos;
-    float3 ambient;
-    float3 diffuseColor;
-    float  diffuseIntensity;
-    float attConst;
-    float attLin;
-    float attQuad;
+	float3 lightPos;            //광원위치
+    float3 ambient;             //주변광
+    float3 diffuseColor;        //확산광
+    float  diffuseIntensity;    //확산광 세기
+    float attConst;             //빛감쇄 상수
+    float attLin;               //빛감쇄 거리
+    float attQuad;              //빛 감쇄 정방체
 };
 
+//오브젝트 고유의 색과 빛나는정도를 정한다.
 cbuffer ObjectCBuf
 {
-    float3 materialColor;
+    float3  materialColor;      //머터리얼 색
+    float   specularIntensity;  //정반사 강도
+    float   specularPower;      //정반사 승수
 };
 
 
@@ -23,12 +26,23 @@ float4 main(float3 worldPos : POSITION, float3 n : NORMAL) : SV_TARGET
     const float distToL = length(vToL);
 	//정점부터 광원까지의 방향벡터
     const float3 dirToL = vToL / distToL;
-	// 빛 섞기
+	//***점광원***//
+    // 빛 섞기
     const float att = 1.0f / (attConst + attLin * distToL + attQuad * (distToL * distToL));
 	// 빛 강도
     const float3 diffuse = diffuseColor * diffuseIntensity * att * max(0.0f, dot(dirToL, n));
+    //************//
+
+    //***정반사***//
+    //빛벡터에 대한 반사벡터
+    const float3 w = n * dot(dirToL, n);
+    const float3 r = w * 2.0f - dirToL;
+    //시야벡터와 빛반사벡터 사이의 각도를 기반으로 정반사의 강도를 제곱수와 함께 구한다
+    const float3 specular = (diffuseColor * diffuseIntensity) * specularIntensity * pow(max(0.0f, dot(normalize(r), normalize(worldPos))), specularPower);
+    //************//
+
 	// 최종 색
-    return float4(saturate(diffuse+ambient)*materialColor,1.0f);
+    return float4(saturate(diffuse+ambient+specular)*materialColor,1.0f);
 }
 
 //attenuation 빛 감쇠의 적절한 값을 알고싶으면 아래사이트를 참고할것
